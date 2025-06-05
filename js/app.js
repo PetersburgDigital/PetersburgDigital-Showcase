6f47c1e1e0703f65cb057908225236226fea8b73
Анастасия <road.iknow1971@gmail.com>
2025-06-05 20:40:59 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,41 +49,41 @@ listElements.forEach((click) => {
       sleep(3000).then(() => { 
         listElements[0].classList.remove('child--active')
         listElements[1].classList.add('child--active')
-        blockImg.style.backgroundImage = 'url(../img/kuchenland.png)'
+        blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/kuchenland.png)'
      });
      sleep(7000).then(() => { 
         listElements[1].classList.remove('child--active')
         listElements[2].classList.add('child--active')
-        blockImg.style.backgroundImage = 'url(../img/ralph-lauren.png)'
+        blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/ralph-lauren.png)'
      });
      sleep(11000).then(() => { 
         listElements[2].classList.remove('child--active')
         listElements[3].classList.add('child--active')
-        blockImg.style.backgroundImage = 'url(../img/easyDrive.png)'
+        blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/easyDrive.png)'
      });
      sleep(15000).then(() => { 
         listElements[3].classList.remove('child--active')
         listElements[4].classList.add('child--active')
-        blockImg.style.backgroundImage = 'url(../img/movie-night.png)'
+        blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/movie-night.png)'
      });
    })
 
 
     click.addEventListener('mouseover', () => {
         if (click === listElements[0]) {
-            blockImg.style.backgroundImage = 'url(../img/real-estate.jpeg)'
+            blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/real-estate.jpeg)'
         }
         else if (click === listElements[1]) {
-            blockImg.style.backgroundImage = 'url(../img/kuchenland.png)'
+            blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/kuchenland.png)'
         }
         else if (click === listElements[2]) {
-            blockImg.style.backgroundImage = 'url(../img/ralph-lauren.png)'
+            blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/ralph-lauren.png)'
         }
         else if (click === listElements[3]) {
-            blockImg.style.backgroundImage = 'url(../img/easyDrive.png)'
+            blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/easyDrive.png)'
         }
         else if (click === listElements[4]) {
-            blockImg.style.backgroundImage = 'url(../img/movie-night.png)'
+            blockImg.style.backgroundImage = 'url(PetersburgDigital-Showcase/img/movie-night.png)'
         }
 
         listElements.forEach((remove) => {
